d8c7706cc0d5910408e4e20222f1a12de457b327
andy <yuniugi1234@gmail.com>
2025-03-20 22:36:46 +0900

    init project

---
+++ src/main/java/com/todo/todo_api_server/TodoApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.todo.todo_api_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiServerApplication.class, args);
+	}
+
+}
